29bcf552a469ba63fc1761fd946ddab481222f99
root <root@web01>
2025-01-16 17:35:21 -0600

    handle http server

--- main.go
+++ main.go
@@ -5,35 +5,39 @@ import (
 	"net/http"
 )
 
+// Handler functions for different pages
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
-
+	// Define the route handlers
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
+	// Redirect the root ("/") to "/courses"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/courses", http.StatusFound)
+	})
+
+	// Start the HTTP server
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
